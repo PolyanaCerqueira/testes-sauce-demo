6a36d5771861d3f6fcb8e78bc263c884da212a8f
Polyana Cerqueira <polyanacerqueira078@gmail.com>
2025-02-22 17:58:31 -0300

    feat(login): componentizando login - issue: #6

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+import '../support/commands'
+
 describe('Login no Sauce Demo', () => {
     const users = [
         'standard_user',
@@ -8,37 +10,28 @@ describe('Login no Sauce Demo', () => {
         'visual_user'
     ];
 
-    function login(user) {
-        cy.visit('https://www.saucedemo.com/');
-        cy.get('[data-test="username"]').type(user);
-        cy.get('[data-test="password"]').type('secret_sauce');
-        cy.get('[data-test="login-button"]').click();
-    }
-
-    function verificarLoginSucesso() {
-        cy.url().should('include', '/inventory.html');
-        cy.get('.inventory_list').should('be.visible');
-        cy.log(' Login bem-sucedido');
-    }
-
-    function verificarLoginFalha(user) {
-        cy.get('[data-test="error"]').should('be.visible')
-            .and('contain', 'Epic sadface');
-        cy.log(` Login falhou para ${user}, exibindo mensagem de erro.`);
-        throw new Error(`Teste mal sucedido: Usuário ${user} não conseguiu fazer login.`);
-    }
+    const password = 'secret_sauce'
 
     users.forEach((user) => {
         it(`Deve testar o login com o usuário: ${user}`, () => {
-            login(user);
+            cy.login(user, password)
 
             cy.url().then((url) => {
                 if (url.includes('/inventory.html')) {
-                    verificarLoginSucesso();
+
+                    cy.url().should('include', '/inventory.html');
+                    cy.get('.inventory_list').should('be.visible');
+                    cy.log(' Login bem-sucedido');
                 } else {
-                    verificarLoginFalha(user);
+                    cy.get('[data-test="error"]').should('be.visible')
+                        .and('contain', 'Epic sadface');
+
+                    cy.log(` Login falhou para ${user}, exibindo mensagem de erro.`);
+
+                    throw new Error(`Teste mal sucedido: Usuário ${user} não conseguiu fazer login.`);
                 }
             });
         });
     });
+
 });
