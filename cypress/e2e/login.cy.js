390e7d56431a8d03b41722684217e836f7d453c6
Polyana Cerqueira <polyanacerqueira078@gmail.com>
2025-02-22 18:03:30 -0300

    doc(login): criando comentários - issue #6

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,19 +14,20 @@ describe('Login no Sauce Demo', () => {
 
     users.forEach((user) => {
         it(`Deve testar o login com o usuário: ${user}`, () => {
+            //Chamando componente de login
             cy.login(user, password)
 
             cy.url().then((url) => {
                 if (url.includes('/inventory.html')) {
 
+                    // Verificando se é possível entrar na tela de produtos ao realizar o login
                     cy.url().should('include', '/inventory.html');
                     cy.get('.inventory_list').should('be.visible');
                     cy.log(' Login bem-sucedido');
                 } else {
+                    //Verificando se houve problema nas credenciais do login
                     cy.get('[data-test="error"]').should('be.visible')
                         .and('contain', 'Epic sadface');
-
-                    cy.log(` Login falhou para ${user}, exibindo mensagem de erro.`);
 
                     throw new Error(`Teste mal sucedido: Usuário ${user} não conseguiu fazer login.`);
                 }
